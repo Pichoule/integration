e71a110e678e19999cbae110108d9e4827bb0b8f
GOJON Florian <florian.gojon@epsi.fr>
2018-02-21 16:22:18 +0100

    Init

---
+++ integrationProjet/connexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace integrationToka.Integrationjenkins
+{
+    class Connexion
+    {
+
+        public List<Infirmiere> ListInfirmiere { get => ListInfirmiere; set => ListInfirmiere = value; }
+
+        public void entrerInfirmiereRegister()
+        {
+            string login = Console.ReadLine();
+            string mdp = Console.ReadLine();
+            string nom = Console.ReadLine();
+            string prenom = Console.ReadLine();
+
+            register(login, mdp, nom, prenom);
+        }
+
+        protected void register(string login, string mdp, string nom, string prenom)
+        {
+            ListInfirmiere.Add(login, mdp, nom, prenom);
+        }
+
+        protected void connexion()
+        {
+            string login = Console.ReadLine();
+            string mdp = Console.ReadLine();
+
+            bool droitConnexion = false;
+            foreach (var infirmiere in ListInfirmiere)
+            {
+                if (ListInfirmiere.login == login & ListInfirmiere.mdp == mdp)
+                {
+                    droitConnexion = true;
+                }
+            }
+        }
+    }
+}
---
+++ integrationProjet/infirmiere.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integrationToka.Integrationjenkins
+{
+    class Infirmiere
+    {
+        protected enum Genre { F, M };
+
+        protected string Nom;
+        protected string Prenom;
+
+        protected string Login;
+        protected string Mdp;
+
+        public List<Patient> ListPatients { get; set; }
+
+
+        protected void CreerPatient()
+        {
+            string name = Console.ReadLine();
+            string firstname = Console.ReadLine();
+            string numberSecu = Console.ReadLine();
+            Genre sex = Console.ReadLine();
+            string pathology = Console.ReadLine();
+            DateTime lastVisit = Console.ReadLine();
+
+            protected Patient newPatient = new Patient(name, firstname, sexe, pathology, lastVisit);
+    }
+}
+}

c8354d393515463d53d9d34bde0fb30309ee35fd
Matthieu <matth.epsi@gmail.com>
2018-03-08 12:52:38 +0100

    Merge des projets

---
+++ Plateforme_Gestion_Client/Plateforme_Gestion_Client/Infirmiere.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plateforme_Gestion_Client
+{
+    class Infirmiere
+    {
+        protected enum Genre { F, M };
+
+        protected string Nom;
+        protected string Prenom;
+
+        protected string Login;
+        protected string Mdp;
+
+        public List<Patient> ListPatients { get; set; }
+
+        protected void CreerPatient()
+        {
+            String name = Console.ReadLine;
+            String firstname = Console.ReadLine;
+            String numberSecu = Console.ReadLine;
+            Genre sex = Console.ReadLine;
+            String pathology = Console.ReadLine;
+            DateTime lastVisit = Console.ReadLine;
+
+            protected Patient newPatient = new Patient(name, firstname, sexe, pathology, lastVisit);
+    }
+}
+}
---
+++ Plateforme_Gestion_Client/Plateforme_Gestion_Client/Window3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Plateforme_Gestion_Client
+{
+    /// <summary>
+    /// Logique d'interaction pour Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Inscription(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
